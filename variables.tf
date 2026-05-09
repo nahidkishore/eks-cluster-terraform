d654bcb8221b5586749eb6f2ddbfb4e42ac9f120
Nahidul Islam <nahidkishore99@gmail.com>
2026-05-09 11:01:51 +0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "three-tier-app-cluster"
+}
+
+variable "ssh_key_name" {
+  description = "The name of the SSH key pair to use for instances"
+  type        = string
+  default     = "nahid-ssh-key"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24"]
+}
